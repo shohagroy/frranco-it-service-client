8295b30b9803c85afaa317d7905ac2d195fffa55
shohagroy <pkshohag240@gmail.com>
2023-12-18 21:29:31 +0600

    initial react app and mui setup added

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { Button } from "@mui/material";
+
+function App() {
+  return (
+    <>
+      <main>
+        <h2 style={{ color: "red" }}>hello woeld</h2>
+        <Button variant="contained">Hello world</Button>
+      </main>
+    </>
+  );
+}
+
+export default App;
---
+++ src/theme/theme.tsx
@@ -0,0 +1,16 @@
+import { createTheme } from "@mui/material/styles";
+
+// A custom theme for this app
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#3A40FB",
+      light: "#42D7F8",
+    },
+    secondary: {
+      main: "#19857b",
+    },
+  },
+});
+
+export default theme;
